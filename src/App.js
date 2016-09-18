7f0967ace5735cc8cb0562242da55d4a88661a49
Joy Rimchala <Joy_Rimchala@intuit.com>
2016-09-18 15:20:19 -0700

    add routine to parse data from localStorage to plot

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 import ReactDOM from 'react-dom'
-import logo from './logo.svg';
-import './App.css';
-import rd3 from 'rd3';
+import logo from './logo.svg'
+import readLocalStorageToDict from './readFieldsFromLocalStorage.jsx'
+import './App.css'
+import rd3 from 'rd3'
+import d3 from 'd3'
+import randomBates from 'd3-random'
+// import DataFrame from 'dataFrame'
 
 class App extends Component {
   render() {
@@ -20,36 +24,76 @@ class App extends Component {
   }
 }
 
+// Routine for getting data from localStorage to BarChart (need to refactor)
+// Read data from localStorage
+var fields = ["price", "bedrooms", "bathrooms",  "building_size"]
+var dictData = readLocalStorageToDict(fields)
+var data = dictData['price']
+
+var formatCount = d3.format(",.0f");
+var margin = {top: 10, right: 30, bottom: 30, left: 30},
+    width = 960 - margin.left - margin.right,
+    height = 500 - margin.top - margin.bottom;
+
+function getYScale(data, height) {
+  return d3.scale.linear().
+    domain([0, d3.max(data, (d) => d.y)]).
+    range([height, 0]);
+}
+
+function getXScale(data, width) {
+  return d3.scale.linear().
+    domain([0, d3.max(data)]).
+    range([0, width]);
+}
+
+function parseHistogramData(data) {
+  var plotDict;
+  var plotDictArray = histogramData.map(d => 
+    plotDict = {"x":d.x, "y":d.length}
+  )
+  return plotDictArray
+}
+
+var x = d3.scale.linear().rangeRound([0, width]);
+var xScale = getXScale(data, width);
+var histogramDataFn = d3.layout.histogram().bins(xScale.ticks(20));
+// This is the data we parse to React component
+var histogramData = histogramDataFn(data);
+var yScale = getYScale(histogramData, height);
+var bins = d3.layout.histogram().bins(xScale.ticks(20));
+
+var barValues = parseHistogramData(histogramData)
+var barData = [
+  {
+    "name": "price",
+    "values": barValues
+  }
+]
+
 /* 2nd component - barChart*/
 var barData = [
   {
     "name": "91130",
     "values": [
-      { "x": 10000, "y":  191},
-      { "x": 20000, "y": 390},
-      { "x": 30000, "y": 725},
-      { "x": 40000, "y": 512},
-      { "x": 50000, "y": 110},
-    ]
-  },
-  {  
-    "name": "94190",
-    "values": [
-      { "x": 10000, "y":  9},
-      { "x": 20000, "y": 190},
-      { "x": 30000, "y": 125},
-      { "x": 40000, "y": 215},
-      { "x": 50000, "y": 30},
-    ]
-  },
-  {  
-    "name": "94110",
-    "values": [
-      { "x": 10000, "y":  91},
-      { "x": 20000, "y": 290},
-      { "x": 30000, "y": 125},
-      { "x": 40000, "y": 15},
-      { "x": 50000, "y": 210},
+      { "x": 0, "y": 0},
+      { "x": 100000, "y": 314},
+      { "x": 200000, "y": 138},
+      { "x": 300000, "y": 0},
+      { "x": 400000, "y": 0},
+      { "x": 500000, "y": 0},
+      { "x": 600000, "y": 0},
+      { "x": 700000, "y": 0},
+      { "x": 800000, "y": 0},
+      { "x": 900000, "y": 0},
+      { "x": 1000000, "y": 0},
+      { "x": 1100000, "y": 0},
+      { "x": 1200000, "y": 0},
+      { "x": 1300000, "y": 0},
+      { "x": 1400000, "y": 0},
+      { "x": 1500000, "y": 0},
+      { "x": 1600000, "y": 0},
+      { "x": 1700000, "y": 1},
     ]
   }
 ];
--- src/readFieldsFromLocalStorage.jsx
+++ src/readFieldsFromLocalStorage.jsx
@@ -1,6 +1,20 @@
-function readFieldsFromLocalStorage(fields, numElement=500) {
-	allData = new Array()
-	keys = Object.keys(localStorage)
+var DataFrame = require('dataframe')
+
+var dimensions = [
+  {value: 'firstName', title: 'First Name'},
+  {value: 'lastName', title: 'Last Name'},
+  {value: 'state', title: 'State'},
+  {value: function(row) {
+    return row.transaction.business
+  }, title: 'Business'},
+  {value: function(row) {
+    return row.transaction.type
+  }, title: 'Transaction Type'}
+]
+
+function readLocalStorageToArray(fields, numElement=500) {
+	var allData = new Array()
+	var keys = Object.keys(localStorage)
 	let cnt = 0
 	for (k in keys) {
 		try {
@@ -14,5 +28,23 @@ function readFieldsFromLocalStorage(fields, numElement=500) {
 	return allData
 }
 
-var fields = ["price", "bedrooms", "bathrooms",  "building_size"]
-var fieldData = readFieldsFromLocalStorage(fields)
+function readLocalStorageToDict(fields, numElement=500) {
+	var dataDict = {}
+	fields.map(f => dataDict[f] = [])
+	var keys = Object.keys(localStorage)
+	let cnt = 0
+	for (k in keys) {
+		try {
+			raw = JSON.parse(localStorage.getItem(keys[k])) 
+			if (typeof raw[fields[0]] !== 'undefined' && cnt <= Math.min(numElement, keys.length)) {
+				fields.map(f => 
+					dataDict[f] = dataDict[f].concat(raw[f])
+					)
+				cnt += 1
+			}
+		} catch(err) { console.log(err) }
+	}
+	return dataDict
+}
+
+export default readLocalStorageToDict
